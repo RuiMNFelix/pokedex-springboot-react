8219551758f367064473234783d89cf1f50c3fa7
RMNFelix <1230519@isep.ipp.pt>
2025-09-03 16:37:19 +0100

    Development of US06 #6

--- backend/src/main/java/com/example/pokedex/application/controller/TeamController.java
+++ backend/src/main/java/com/example/pokedex/application/controller/TeamController.java
@@ -4,10 +4,13 @@ import com.example.pokedex.application.service.PokemonService;
 import com.example.pokedex.domain.Pokemon;
 import com.example.pokedex.repository.PokemonRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/my-pokemon")
 public class TeamController {
@@ -20,13 +23,20 @@ public class TeamController {
     }
 
     @GetMapping
-    public List<Pokemon> listAllTasks(){
-        return repository.findAll();
+    public ResponseEntity<List<Pokemon>> listAllTasks(){
+        var body = repository.findAll();
+        return ResponseEntity.ok(body);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Pokemon addNewPokemon(@RequestBody Pokemon newPokemon) {
         return repository.save(newPokemon);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePokemon(@RequestBody Pokemon deletedPokemon) {
+        repository.delete(deletedPokemon);
     }
 }
